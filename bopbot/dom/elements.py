69bf062ace780d94fedf89652328ec07c6174286
Daniel Pareja <ParejaDan@gmail.com>
2020-06-06 11:52:54 -0400

    introduce labeled selectors

--- bopbot/dom/elements.py
+++ bopbot/dom/elements.py
@@ -14,6 +14,15 @@ def validate_dom_hierarchy(dom_hierarchy: [str]):
     return True
 
 
+def validate_label_name(label):
+    if not isinstance(label, str) or not label.replace("_", "").isalpha():
+        raise SelectorError(
+            f"label [{label}] must be str and only have alphabetical chars after label.replace('_', '')"
+        )
+
+    return True
+
+
 class BaseSelector:
     """
     Resource for defining a dom's CSS selector path in list form.
@@ -71,3 +80,73 @@ class BaseSelector:
     @property
     def is_empty(self):
         return len(self._dom_hierarchy) == 0
+
+
+class LabeledSelector(BaseSelector):
+    """
+    Resource for defining dom paths with a human readable label describing
+    the dom object
+    """
+    def __init__(self, label: str, dom_hierarchy: [str]):
+        """
+        Parameters
+        ==========
+        label: Human readable name representation for dom object.
+               Label must be str and only have alphabetical chars after label.replace('_', '').
+        dom_hierarchy: list of HTML/DOM tags in order representing path to selector.
+                       Last element assumed to be target dom element.
+        """
+        validate_label_name(label=label)
+        super().__init__(dom_hierarchy=dom_hierarchy)
+        self.label = label
+
+
+def flatten_selector_hierarchy(selector_hierarchy: []) -> []:
+    """
+    For a given list of items we unpack them to an extended
+    list of objects that can be manipulated by BaseSelector.flatten_hierarchy(..)
+
+    Parameters
+    ==========
+    selector_hierarchy: [
+        BaseSelector,
+        LabeledSelector,
+        [str],
+        str,
+    ]
+
+    Returns
+    =======
+    list items where validate_dom_hierarchy(...) will evaluate true against
+    """
+    flattened_hierarchy = []
+    for item in selector_hierarchy:
+        if isinstance(item, BaseSelector):
+            flattened_hierarchy.extend(item._dom_hierarchy)
+        elif isinstance(item, list):
+            flattened_hierarchy.extend(item)
+        else:
+            flattened_hierarchy.append(item)
+    validate_dom_hierarchy(dom_hierarchy=flattened_hierarchy)
+
+    return flattened_hierarchy
+
+
+def create_labeled_selector(label, selector_hierarchy: []) -> LabeledSelector:
+    return LabeledSelector(
+        label=label,
+        dom_hierarchy=flatten_selector_hierarchy(selector_hierarchy=selector_hierarchy),
+    )
+
+
+def add_selector_to(obj, label: str, selector_hierarchy: []):
+    """
+    for a given object we create a DOMSelector instance with the specified label
+    and hierarchy, then add obj an attribute named with the passed label value
+
+    @obj: object we want to add this selector attribute to
+    @label: camelcase string describing the object the passed dom path hierarchy points to
+    @selector_hierarchy: DOM path that points to selector label describes
+    """
+    selector = create_labeled_selector(label=label, selector_hierarchy=selector_hierarchy)
+    setattr(obj, label, selector)
--- bopbot/tests/unit_tests/dom/test_elements.py
+++ bopbot/tests/unit_tests/dom/test_elements.py
@@ -1,6 +1,14 @@
 import pytest
+from mock import Mock, patch
 
-from bopbot.dom.elements import BaseSelector
+from bopbot.dom.elements import (
+    BaseSelector,
+    LabeledSelector,
+    flatten_selector_hierarchy,
+    create_labeled_selector,
+    validate_label_name,
+    add_selector_to,
+)
 from bopbot.dom.exceptions import SelectorError
 
 
@@ -20,3 +28,53 @@ class TestBaseSelector:
         selector.set_hierarchy(dom_hierarchy=1)
         with pytest.raises(SelectorError):
             selector.to_query()
+
+
+def test_flatten_selector_hierarchy():
+    expected = "my > name > is > bobby"
+    hierarchy = [
+        BaseSelector(dom_hierarchy=["my"]),
+        LabeledSelector(label="bob_button", dom_hierarchy=["name", "is"]),
+        [],
+        "bobby",
+    ]
+    selector = BaseSelector(
+        dom_hierarchy=flatten_selector_hierarchy(selector_hierarchy=hierarchy)
+    )
+    assert expected == selector.to_str()
+
+
+def test_create_labeled_selector_validates():
+    validate_mock = Mock()
+    hierarchy = ["body", "button"]
+    with patch(
+        target="bopbot.dom.elements.validate_dom_hierarchy",
+        new=validate_mock
+    ):
+        create_labeled_selector(label="signup_button", selector_hierarchy=hierarchy)
+        validate_mock.assert_called_with(dom_hierarchy=hierarchy)
+
+
+class TestLabelSelector:
+    def test_rasies_selector_error_with_invalid_label(self):
+        with pytest.raises(SelectorError):
+            LabeledSelector(label=" _catdog", dom_hierarchy=["a"])
+
+        with pytest.raises(SelectorError):
+            LabeledSelector(label=" catdog", dom_hierarchy=["a"])
+
+        with pytest.raises(SelectorError):
+            LabeledSelector(label="cat dog!", dom_hierarchy=["a"])
+
+        with pytest.raises(SelectorError):
+            LabeledSelector(label="catdog!", dom_hierarchy=["a"])
+
+        selector = LabeledSelector(label="cat_dog", dom_hierarchy=["a"])
+
+        assert validate_label_name(selector.label)
+
+    def test_add_labeled_selector_to(self):
+        label_name = "billy_jenkins_selector"
+        add_selector_to(self, label=label_name, selector_hierarchy=["a"])
+
+        assert hasattr(self, label_name)
