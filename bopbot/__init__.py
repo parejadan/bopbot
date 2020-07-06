f3d3d67fea4da0f4a42f0d9ed44fc185ec0c2702
Daniel Pareja <ParejaDan@gmail.com>
2020-07-05 20:38:50 -0400

    publish pypi version 0.1.1

--- bopbot/__init__.py
+++ bopbot/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "0.1.2"
+
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    install_requires=[
+        "asyncio==3.4.3",
+        "pyppeteer==0.2.2",
+        "user-agent==0.1.9",
+        "psutil==5.7.0",
+    ],
+    include_package_data=True,
+    packages=setuptools.find_packages(exclude=("*.tests.*",)),
+    package_data={
+        "bopbot": ["static/*"]
+    }
+)
