390b045c7998f693391c2d6f565220c7f6b9940e
Daniel Pareja <ParejaDan@gmail.com>
2020-07-11 20:53:12 -0400

    publish pypi version 0.1.7
    - include json logging library to setup install_requires

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setuptools.setup(
         "pyppeteer==0.2.2",
         "user-agent==0.1.9",
         "psutil==5.7.0",
+        "python-json-logger==0.1.11",
     ],
     include_package_data=True,
     packages=setuptools.find_packages(exclude=("*.tests.*",)),
